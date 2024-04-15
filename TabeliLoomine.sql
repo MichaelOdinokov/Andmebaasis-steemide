9ac798b301d1ca10d91a3617eff5bc702b4034c9
MichaelOdinokov <120027502+MichaelOdinokov@users.noreply.github.com>
2024-04-15 11:19:20 +0300

    Update TabeliLoomine.sql

--- TabeliLoomine.sql
+++ TabeliLoomine.sql
@@ -79,29 +79,14 @@ SELECT * FROM opilane;
 INSERT INTO hinnang(kuupaev, opilaneid,rühmajuhataja, hinnang)
 VALUES ('12.09.2024','1', '1','vaga hasti');
 
---не работает послелдняя таблица
 
 
 
-CREATE TABLE Language
-(
-ID int NOT NULL PRIMARY KEY,
-Code char(3) NOT NULL,
-Language varchar(50) NOT NULL,
-IsOfficial bit,
-Percentage decimal(5,2)
-); 
---tabeli andmete lisamine
-SELECT * FROM Language;
-INSERT INTO Language (ID, Code, Language, IsOfficial, Percentage)
-VALUES (100, 'EST', 'Eesti', 1, 80.5)
--- TABELI STRUKTUURI MUUTMINE 
-ALTER TABLE Language ADD Capital varchar(20);
---veergu kustutamine 
-ALTER TABLE Language DROP COLUMN Capital;
-ALTER TABLE Language ADD Capital varchar(20);
-INSERT INTO Language (ID, Code, Language, IsOfficial, Percentage, Capital)
-VALUES (101, 'FIN', 'Soome', 1, 90.5, 'Helsinki');
---uuendamine 
-UPDATE Language SET Capital='Tallinn'
-WHERE ID=100;
+CREATE TABLE opilaned(
+opilaneId int Primary Key identity(1,1),
+opilaneNimi varchar(10))
+SELECT * FROM opilaned
+
+INSERT INTO opilaned(opilaneNimi)
+VALUES ('Artemi');
+SELECT * FROM opilaned;
