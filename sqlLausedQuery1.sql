46b9bf207687dca9e46ac4f6e8739daa5da793d0
Michael Odinokov <mihhail.odinokov@gmail.com>
2024-05-08 12:41:45 +0300

    Vali inimesed nime järgi nende nimest 5 tähega

--- sqlLausedQuery1.sql
+++ sqlLausedQuery1.sql
@@ -127,7 +127,7 @@ select * from Person where Email not like '%@%'
 -- ainult üks täht
 select * from Person where Email like '_@_.com'
 
---?
+--Vali inimesed nime järgi nende nimest 5 tähega
 select * from Person where Name like '[^WAS]%'
 --- ?
 select * from Person where (City = 'Gotham' or City = 'New York')
