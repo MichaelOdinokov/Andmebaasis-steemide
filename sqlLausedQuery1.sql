b50b3c96d84acfd10d1b2dfb24a13ed4abe754f8
Michael Odinokov <mihhail.odinokov@gmail.com>
2024-05-08 12:38:04 +0300

    vaadake tabelit 2 ja kustutage isiku ID 8

--- sqlLausedQuery1.sql
+++ sqlLausedQuery1.sql
@@ -91,7 +91,7 @@ add constraint CK_Person_Age check (Age > 0 and Age < 150)
 insert into Person (Id, Name, Email, GenderId, Age)
 values (9, 'Test', 'Test', 2, 120)
 
---?
+--vaadake tabelit 2 ja kustutage isiku ID 8
 select * from Person
 go
 delete from Person where Id = 8
