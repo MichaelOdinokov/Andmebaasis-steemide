b5f247615ff3c94d1255dc99060ad5276bfa721e
Michael Odinokov <mihhail.odinokov@gmail.com>
2024-05-08 13:34:09 +0300

    Töötaja number küsib tema nime

--- sqlLausedQuery1.sql
+++ sqlLausedQuery1.sql
@@ -351,11 +351,11 @@ declare @FirstName nvarchar(50)
 execute spGetNameById1 6, @FirstName output
 print 'Name of the employee = ' + @FirstName
 
---?
+--Töötaja number küsib tema nime
 create proc spGetNameById2
 @Id int
 as begin
-	return (select FirstName from Employees where Id = @Id)
+	return (select Name from Employees where Id = @Id)
 end
 
 -- ?
